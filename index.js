1e7862e34e69c3d685e9a6e90208a0ba1d99402c
nancylee713 <srlee713@gmail.com>
2019-11-09 12:51:28 -0700

    Add a get route to /api/v1/papers

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.get('/', (req, res) => {
   res.send('Hello, publications');
 })
 
+app.get('/api/v1/papers', (req, res) => {
+  database('papers').select()
+    .then((papers) => {
+      res.status(200).json(papers);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
